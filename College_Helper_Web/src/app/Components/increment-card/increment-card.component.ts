2cc5f441b0ff201cea9b734b6731e5a4d26ff5f7
Kiwifan1 <joshua.venable@outlook.com>
2024-02-14 12:51:44 -0800

    json up and working

--- College_Helper_Web/src/app/Components/increment-card/increment-card.component.ts
+++ College_Helper_Web/src/app/Components/increment-card/increment-card.component.ts
@@ -7,7 +7,11 @@ import { ApiCallService } from 'src/app/Services/api-call.service';
   styleUrls: ['./increment-card.component.scss'],
 })
 export class IncrementCardComponent {
-  number = 0;
+  number: number = 0;
+  name: string = '';
+  age: number = 0;
+  agePressed: boolean = false;
+  result: any;
 
   constructor(private apiCallService: ApiCallService) {}
 
@@ -15,6 +19,13 @@ export class IncrementCardComponent {
     console.log('clicked!');
     this.apiCallService.incrementNumber(this.number).subscribe((data: any) => {
       this.number = data['result'];
+    });
+  }
+
+  getAge() {
+    this.apiCallService.getAge(this.name, this.age).subscribe((data: any) => {
+      this.agePressed = true;
+      this.result = data;
     });
   }
 }
