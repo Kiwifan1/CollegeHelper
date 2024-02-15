15e8ff1342c0700ebd5df9bbb071641d3df3e6bd
Kiwifan1 <joshua.venable@outlook.com>
2024-02-15 11:19:43 -0800

    adding functions

--- College_Helper_Api/function_app.py
+++ College_Helper_Api/function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
@@ -20,6 +22,32 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
+
+
+@app.route(route="increment/{number}", methods=["GET"])
+def increment(req: func.HttpRequest) -> func.HttpResponse:
+    if not req.route_params.get('number') or not req.route_params.get('number').isdigit():
+        return func.HttpResponse("Please provide a number in the URL", status_code=400)
+    number = int(req.route_params.get('number'))
+    response = {
+        "value": number + 1
+    }
+    return func.HttpResponse(json.dumps(response), status_code=200)
+
+
+@app.route(route="getAge", methods=["POST"])
+def get_age(req: func.HttpRequest) -> func.HttpResponse:
+    if not req.get_json():
+        return func.HttpResponse("Please provide a name and age in the request body", status_code=400)
+    req_body = req.get_json()
+    name = req_body.get('name')
+    age = req_body.get('age')
+
+    response = {
+        "name": name,
+        "age": age + 1
+    }
+    return func.HttpResponse(json.dumps(response), status_code=200)
