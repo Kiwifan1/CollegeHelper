21d2a2306d4a3efe4f5a347b848b653b77cc0615
Joshua Venable <70110252+Kiwifan1@users.noreply.github.com>
2024-03-26 15:05:06 -0700

    working bind

--- College_Helper_Api/scholarship_db_triggers.py
+++ College_Helper_Api/scholarship_db_triggers.py
@@ -8,7 +8,9 @@ import azure.functions as func
 from azure.core.paging import ItemPaged, PageIterator
 from azure.cosmos import CosmosClient, ContainerProxy
 from http import HTTPStatus
-from user_db_triggers import CLIENT, DATABASE, query_cosmos_db
+from user_db_triggers import CLIENT, DATABASE, query_cosmos_db, hash
+
+from model.clustering import predict_scholarship_scores
 
 schol_bp = func.Blueprint()
 cosmos_db_connection = "CosmosDBConnectionString"
@@ -119,3 +121,81 @@ def get_num_scholarships(req: func.HttpRequest) -> func.HttpResponse:
         )
     except Exception as e:
         return func.HttpResponse(f"Error: {str(e)}", status_code=500)
+
+
+@schol_bp.queue_output(
+    arg_name="msg",
+    queue_name="scholarship-predictions-queue",
+    connection="AzureWebJobsStorage",
+)
+@schol_bp.route(route="predict_scholarships", methods=["POST"])
+def predict_scholarships(
+    req: func.HttpRequest,
+    msg: func.Out[func.QueueMessage],
+) -> func.HttpResponse:
+    user = json.loads(req.get_body().decode("utf-8"))
+    if not user:
+        return func.HttpResponse("Error: Missing user data", status_code=400)
+    if not "id" in user:
+        return func.HttpResponse("Error: Missing user id", status_code=400)
+    if not "password" in user or not "salt" in user:
+        return func.HttpResponse("Error: Missing password/salt", status_code=400)
+
+    potential_pass = hash(user["password"], user["salt"])
+
+    # check if user exists
+    query = "SELECT * FROM c WHERE c.id = @id AND c.password = @password"
+    params = [
+        {"name": "@id", "value": user["id"]},
+        {"name": "@password", "value": potential_pass},
+    ]
+
+    try:
+        user = list(
+            query_cosmos_db(query, params, DATABASE.get_container_client("USER"), True)
+        )
+        if not user:
+            return func.HttpResponse("Error: User not found", status_code=404)
+    except Exception as e:
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
+
+    msg.set(json.dumps(user[0]))
+    return func.HttpResponse("User found", status_code=200)
+
+
+@schol_bp.queue_trigger(
+    arg_name="msg",
+    queue_name="scholarship-predictions-queue",
+    connection="AzureWebJobsStorage",
+)
+@schol_bp.cosmos_db_input(
+    arg_name="scholarships",
+    database_name="CollegeHelperDB",
+    container_name="SCHOLARSHIP",
+    connection=cosmos_db_connection,
+)
+@schol_bp.cosmos_db_output(
+    arg_name="updatedUser",
+    database_name="CollegeHelperDB",
+    container_name="USER",
+    connection=cosmos_db_connection,
+)
+def process_prediction_request(
+    msg: func.QueueMessage,
+    scholarships: func.DocumentList,
+    updatedUser: func.Out[func.Document],
+) -> None:
+    if not msg or not msg.get_body() or not scholarships:
+        return
+
+    user = json.loads(msg.get_body().decode("utf-8"))
+    scholarships = [s.data for s in scholarships]
+
+    user_preds = predict_scholarship_scores(user, scholarships)
+
+    # update user with new predictions
+    user["scholarshipScores"] = user_preds
+    updatedUser.set(user)
+
+    logging.info(f"Updated user: {user}")
+    pass
