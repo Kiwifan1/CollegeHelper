5e5ca913c5f7a927ffbad34d1e649007e0172bdd
Joshua Venable <70110252+Kiwifan1@users.noreply.github.com>
2024-04-30 20:23:05 -0700

    Highschool info (#69)
    * fixed highschools
    
    * added high school to user
    
    * fixed bugs

--- College_Helper_Web/src/app/Components/questionnaire-stepper/highschool/highschool.component.ts
+++ College_Helper_Web/src/app/Components/questionnaire-stepper/highschool/highschool.component.ts
@@ -20,37 +20,58 @@ export class HighschoolComponent implements OnInit {
   );
 
   highschools!: Highschool[];
+
+  filteredStates: Observable<string[]> = of(this.stateEnums);
   filteredHighschools: Observable<Highschool[]> = of(this.highschools);
-  highschoolSearchForm = new FormControl(['']);
+
+  highschoolSearchForm = new FormControl('');
+  stateSearchForm = new FormControl('');
 
   constructor(
     private highschoolService: HighschoolService,
     private loadingService: LoadingService
   ) {}
   ngOnInit(): void {
-    console.log(this.state);
+    this.highschoolSearchForm.valueChanges.subscribe((value) => {
+      this.filterHighschools(value as string);
+    });
+
+    this.stateSearchForm.valueChanges.subscribe((value) => {
+      this.filterStates(value as string);
+    });
+  }
+
+  reset() {
+    this.highschools = [];
+    this.filterHighschools('');
+    this.highschoolForm.patchValue({
+      highschool: null,
+    });
+    this.highschoolSearchForm.setValue('');
   }
 
   getHighschools(state: string) {
     this.state = state;
+    this.reset();
     this.loadingService.updateLoadingStatus(true);
     this.highschoolService.getHighschools(state).subscribe({
       next: (highschools) => {
         this.highschools = highschools;
-        console.log(highschools);
+        this.filterHighschools('');
         this.loadingService.updateLoadingStatus(false);
       },
       error: (error) => {
         console.error(error);
         this.highschools = [];
+        this.filterHighschools('');
         this.loadingService.updateLoadingStatus(false);
       },
     });
   }
 
-  filterHighschools(name: string): Observable<Highschool[]> {
+  filterHighschools(name: string) {
     // Filter the highschools based on the name or code
-    return of(
+    this.filteredHighschools = of(
       this.highschools.filter(
         (highschool) =>
           highschool.name.toLowerCase().includes(name.toLowerCase()) ||
@@ -59,9 +80,23 @@ export class HighschoolComponent implements OnInit {
     );
   }
 
+  filterStates(name: string) {
+    this.filteredStates = of(
+      this.stateEnums.filter((state) =>
+        state.toLowerCase().includes(name.toLowerCase())
+      )
+    );
+  }
+
+  setState(state: string) {
+    this.state = state;
+    this.getHighschools(state);
+  }
+
   setHighschool(highschool: Highschool) {
     this.highschoolForm.patchValue({
       highschool: highschool,
     });
+    console.log(this.highschoolForm.value)
   }
 }
